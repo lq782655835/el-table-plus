447069e619cb33b6e501b741ede85016789270f4
springleo <782655835@qq.com>
2020-05-01 23:39:25 +0800

    refactor: rename variable && update demo

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ use `<el-table-plus>` in your page
 <template>
     <el-table-plus
       :data="list"
-      :fieldList="[
+      :columns="[
         { label: 'ID', value: 'id', width: '80px' },
         { label: '存储卷名', value: 'name', type: 'copy' },
         { label: '总容量', value: 'storage', fn: val => `${val}G` },
@@ -54,50 +54,27 @@ you can get preview by above code：
 
 ### el-table-plus Props
 
-基于el-table，所以Props支持el-form上所有props以及事件event。
+基于el-table，所以Props支持el-table上所有props以及事件event。
 
 Prop | Type | Default | Description
 --- | --- | --- | ---
 loading | Boolean |  | 动效loading
 data | Array |  [] | 列表数据
-fieldList | Array | column item配置列表 | 详细见[如下fieldList Attrs](#fieldList-Attrs)
-handle | Object |  | 操作栏配置,主要针对点击事件，详细见[如下handle Attrs](#handle-Attrs)
-extendData | Object |  | 扩展数据，解决组件只能传Array Table外数据，无法额外传入数据。
+columns | Array | column item配置列表 | 详细见[如下columns Attrs](#columns-Attrs)
 
-### fieldList Attrs
+### columns Attrs
 
 Attr | Type | Default | Description
 --- | --- | --- | ---
 label | String |   | 列名称
 prop | String |   | 列数据字段，支持多层对象嵌套，如user.email.prefix
-fn | Function |   | 自定义内容替换默认value。函数参数(value, row)
+fn | Function |   | 自定义format函数。函数参数(value, row, column, $index)
+customRender | Function |   | 自定义render。函数参数(h, value, row, column, $index)
+hidden | Boolean |   | 是否隐藏该列。建议是一个computed，使得可以响应式显示隐藏
 width | String |   | 列宽度
 minWidth | String | 100px  | 最小列宽度，默认'100px'
 fixed | Boolean |  false | 是否固定列
 show-overflow-tooltip | Boolean | false  | 超出cell时，是否使用tips提示，默认超过显示tips
-hidden | Boolean |   | 是否隐藏该列。建议是一个computed，使得可以响应式显示隐藏
-
-### handle Attrs
-
-Attr | Type | Default | Description
---- | --- | --- | ---
-width | String |   | 操作栏宽度
-minWidth | String |   | 操作栏最小列宽度
-fixed | Boolean |  false | 是否固定列
-btnList | Array |   | 按钮列表。详细见[如下handle btnList Attrs](#handle-btnList-Attrs)
-
-### handle btnList Attrs
-
-Attr | Type | Default | Description
---- | --- | --- | ---
-label | String \| Function |   | button显示名称
-icon | String |   | el-link图标
-btType | String |   | el-link的主题色,默认是primary
-func | Function |   | button单击事件，参数(row, extendData)
-disabled | Boolean \| Function |   | 是否禁用该button
-hidden | Boolean \| Function |   | 是否隐藏该button
-
-> 由于type=slot用到v-slot作用域插槽，vue版本需要v2.6+
 
 ## License
 
--- src/components/el-table-plus.jsx
+++ src/components/el-table-plus.jsx
@@ -1,5 +1,4 @@
 import UCopy from './u-copy'
-import { isFunction } from '@springleo/30-seconds-of-code'
 
 export default {
   name: "el-table-plus",
@@ -11,76 +10,39 @@ export default {
     loading: { type: Boolean },
     // 列表数据
     data: { type: Array, default: () => [] },
-    /**
-     * 表格字段列表配置。以下是每个字段可配置内容：
-     * label：列名称
-     * value：对应字段
-     * fn: 自定义内容
-     * type: 内置规则，有slot、filter、badge
-     * width： 宽度
-     * minWidth：最小宽度
-     * fixed：是否固定
-     * hidden：是否隐藏该列。建议是一个computed，使得可以响应式显示隐藏（此时tableConfig也需要是一个computed）
-     */
-    fieldList: { type: Array, default: () => [] },
-    /****
-     * 操作栏配置,主要针对点击事件
-     * width： 宽度
-     * minWidth：最小宽度
-     * fixed：是否固定
-     * btList: 按钮列表。以下是每个列表可配内容：
-     ** label: 名称，可配置为string或function
-     ** event(移除)：事件名(作用类似上面的value)。详细见func
-     ** func(新增): 按钮处理事件，单独设置事件，不再通过统一的emit发送到父组件处理。为了兼容以前的代码，优先执行func（记住不能同时拥有func和handleClick）
-     ** btType： el-link的主题色,默认是primary
-     ** icon： 图标
-     ** disabled：可配置为boolean或function
-     ** hidden：是否隐藏该列。建议是一个computed，使得可以响应式显示隐藏（此时tableConfig也需要是一个computed）
-     */
-    handle: { type: Object },
-    noData: Object, // {label, href, command}
-    extendData: Object // 组件扩展数据，解决组件只能传Array Table外数据，无法额外传入数据。
-  },
-  methods: {
-    fieldFunc(field, ...args) {
-      if (isFunction(field)) {
-        return field(...args, this.extendData)
-      } else {
-        return field
-      }
-    }
+    columns: { type: Array, default: () => [] }
   },
   render(h) {
-    const getDataValue = (item, row) => item.prop.split(".").reduce((obj, cur) => obj[cur], row);
-    const renderColumns = columns => columns.filter(i => !i.hidden).map((item) => {
+    const getDataValue = (column, row) => column.prop.split(".").reduce((obj, cur) => obj[cur], row);
+    const renderColumns = columns => columns.filter(i => !i.hidden).map((column) => {
       const scopedSlots = {
         default: ({ row, $index}) => {
           // 支持链式. 如：xxx.xxx
-          const defaultValue = getDataValue(item, row)
+          const defaultValue = getDataValue(column, row)
 
           // 预设组件
-          if (item.type === 'copy') {
+          if (column.type === 'copy') {
             return <u-copy label={defaultValue} />
           }
           // 动态组件
-          if (['el-', 'dynamic-'].some(prefix => item.type && item.type.startsWith(prefix))) {
-            return h(item.type, defaultValue)
+          if (['el-', 'dynamic-'].some(prefix => column.type && column.type.startsWith(prefix))) {
+            return h(column.type, { props: column }, defaultValue)
           }
 
           // 自定义组件
-          if (item.customRender) {
-            return item.customRender(h, item, $index, row)
+          if (column.customRender) {
+            return column.customRender(h, defaultValue, row, column, $index)
           }
           // 自定义文字
-          if (item.fn) {
-            return item.fn(defaultValue, row, item)
+          if (column.fn) {
+            return column.fn(defaultValue, row, column, $index)
           }
 
           return defaultValue
         }
       }
 
-      return <el-table-column key={item.prop} {...{props: item}} scopedSlots={scopedSlots} />
+      return <el-table-column key={column.prop} {...{props: column}} scopedSlots={scopedSlots} />
     })
 
     return (<el-table
@@ -89,7 +51,7 @@ export default {
     data={this.data}
     {...{props: this.$attrs, on: this.$listeners}}
   >
-      {renderColumns(this.fieldList)}
+      {renderColumns(this.columns)}
   </el-table>)
   }
 };
